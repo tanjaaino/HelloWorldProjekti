f193f4572efe0530de5e302a27f212fb51f2feec
Bergius Tanja <berta@haaga-helia.fi>
2021-02-04 14:36:38 +0200

    projektin ensimmäiset lisäykset

---
+++ src/main/java/hh/swd20/helloprojekti/HelloprojektiApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.helloprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloprojektiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/hh/swd20/helloprojekti/web/HelloController.java
@@ -0,0 +1,14 @@
+package hh.swd20.helloprojekti.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+@RequestMapping("/hello")
+public String sayHello() {
+	return "hello"; //hello.html
+}
+
+}
